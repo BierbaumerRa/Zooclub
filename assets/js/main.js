c90d0c0c11388da7fd587b26e36c29ba175bc526
BierbaumerRa <133593866+BierbaumerRa@users.noreply.github.com>
2024-03-23 16:39:43 +0100

    Add files via upload

--- assets/js/main.js
+++ assets/js/main.js
@@ -9,14 +9,14 @@ class MyHeader extends HTMLElement {
                     <nav class="main-nav">
                          <!-- ***** Logo Start ***** -->
                         <div>
-                            <a href="index.html" class="logo"><img src="assets/images/Logo_4.png" alt="" witdh="78" height="78"></a>
+                            <a href="index.html" class="logo"><img src="assets/images/Logo_5.png" alt="" witdh="78" height="78"></a>
                         </div>
                         <!-- ***** Logo End ***** -->
                         <!-- ***** Menu Start ***** -->
                         <ul class="nav">
                             <li><a href="index.html" class="active">Home</a></li>
-                            <li><a href="about.html">About Us</a></li>
-                            <li><a href="rent-venue.html">Rent Venue</a></li>
+                            <li><a href="about-us.html">Über Uns</a></li>
+                            <!-- <li><a href="rent-venue.html">Rent Venue</a></li> -->
                         </ul>
                         <a class='menu-trigger'>
                             <span>Menu</span>
@@ -34,13 +34,13 @@ customElements.define('my-header', MyHeader)
 
 class MyFooter extends HTMLElement {
     connectedCallback() {
-        this.innerHTML = `<footer>
+        this.innerHTML = ` <footer>
         <div class="container">
             <div class="row">
                 <div class="col-lg-4">
                     <div class="address">
-                        <h4>ZooClub Address</h4>
-                        <span>5 College St NW, <br>Norcross, GA 30071<br>United States</span>
+                        <h4>ZooClub Addresse</h4>
+                        <span>Waagplatz 7<br>9020 Klagenfurt<br>Österreich</span>
                     </div>
                 </div>
                 <div class="col-lg-4">
@@ -48,45 +48,26 @@ class MyFooter extends HTMLElement {
                 </div>
                 <div class="col-lg-4">
                     <div class="hours">
-                        <h4>Open Hours</h4>
-                        <ul>
-                            <li>Mon to Fri: 10:00 AM to 8:00 PM</li>
-                            <li>Sat - Sun: 11:00 AM to 4:00 PM</li>
-                            <li>Holidays: Closed</li>
-                        </ul>
-                    </div>
-                </div>
-                <div class="col-lg-12">
-                    <div class="under-footer">
-                        <div class="row">
-                            <div class="col-lg-6 col-sm-6">
-                                <p>São Conrado, Rio de Janeiro</p>
-                        </div>
                     </div>
                 </div>
                 <div class="col-lg-12">
                     <div class="sub-footer">
                         <div class="row">
                             <div class="col-lg-3">
-                                <div class="logo"><span>Zoo<em>Club</em></span></div>
+                                <div>
+                                    <a href="index.html" class="logo"><img src="assets/images/Logo_4.png" alt="" witdh="78" height="78"></a>
+                                </div>
                             </div>
                             <div class="col-lg-6">
                                 <div class="menu">
-                                    <ul>
-                                        <li><a href="index.html" class="active">Home</a></li>
-                                        <li><a href="about.html">About Us</a></li>
-                                        <li><a href="rent-venue.html">Rent Venue</a></li>
-                                        <!--<li><a href="shows-events.html">Shows & Events</a></li> 
-                                        <li><a href="tickets.html">Tickets</a></li> -->
-                                    </ul>
                                 </div>
                             </div>
                             <div class="col-lg-3">
                                 <div class="social-links">
                                     <ul>
-                                        <li><a href="#"><i class="fa fa-telegram"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-facebook"></i></a></li>
-                                        <li><a href="#"><i class="fa fa-instagram"></i></a></li>
+                                        <!-- <li><a href="#"><i class="fa fa-telegram"></i></a></li> -->
+                                        <li><a href="https://www.facebook.com/ZooClub9020"><i class="fa fa-facebook"></i></a></li>
+                                        <li><a href="https://www.instagram.com/zoo.club9020?utm_source=qr&igsh=YWRhMTFldTAybWwy"><i class="fa fa-instagram"></i></a></li>
                                     </ul>
                                 </div>
                             </div>
